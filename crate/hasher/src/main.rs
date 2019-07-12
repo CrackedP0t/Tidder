ae9625a19519262c56ad35e48a078feee760f345
Elaina Martineau <elainamartineau@gmail.com>
2019-07-12 01:22:26 -0600

    Don't unwrap in hasher

--- crate/hasher/src/main.rs
+++ crate/hasher/src/main.rs
@@ -2,7 +2,13 @@ use common::*;
 
 fn main() {
     std::env::args().skip(1).fold(None, |last, arg| {
-        let (hash, link, _get_kind) = get_hash(&arg).unwrap();
+        let (hash, link, _get_kind) = match get_hash(&arg) {
+            Ok(res) => res,
+            Err(e) => {
+                println!("{} failed: {}", arg, e);
+                return last;
+            }
+        };
         let mut out = format!("{}: {}", link, hash);
         if let Some(last) = last {
             out = format!("{} ({})", out, distance(hash, last));

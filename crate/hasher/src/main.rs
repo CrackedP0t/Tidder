6bdfd0031be3f18dae5edbc0eca18a0e63957f89
Elaina Martineau <elainamartineau@gmail.com>
2019-07-20 03:10:30 -0600

    Hasher compiles but doesn't run

--- crate/hasher/src/main.rs
+++ crate/hasher/src/main.rs
@@ -1,8 +1,9 @@
 use common::*;
+use futures::future::Future;
 
 fn main() {
     std::env::args().skip(1).fold(None, |last, arg| {
-        let (hash, link, _get_kind) = match get_hash(&arg) {
+        let (hash, link, _get_kind) = match get_hash(arg.clone()).wait() {
             Ok(res) => res,
             Err(e) => {
                 println!("{} failed: {}", arg, e);

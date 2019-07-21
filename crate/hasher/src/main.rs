e84c4c2303611c59a7681d549a0cf58f5475bfb7
Elaina Martineau <elainamartineau@gmail.com>
2019-07-20 19:13:04 -0600

    Fix hasher

--- crate/hasher/src/main.rs
+++ crate/hasher/src/main.rs
@@ -1,21 +1,29 @@
 use common::*;
-use futures::future::Future;
+use futures::{
+    future::{ok, Future},
+    stream::{iter_ok, Stream},
+};
 
 fn main() {
-    std::env::args().skip(1).fold(None, |last, arg| {
-        let (hash, link, _get_kind) = match get_hash(arg.clone()).wait() {
-            Ok(res) => res,
-            Err(e) => {
-                println!("{} failed: {}", arg, e);
-                return last;
-            }
-        };
-        let mut out = format!("{}: {}", link, hash);
-        if let Some(last) = last {
-            out = format!("{} ({})", out, distance(hash, last));
-        }
-        println!("{}", out);
+    let args = std::env::args().skip(1).map(|arg| arg.to_string()).collect::<Vec<String>>();
+    tokio::run(
+        iter_ok::<_, ()>(args).fold(None, |last, arg| {
+            get_hash(arg.clone()).then(move |res| {
+                let (hash, link, _get_kind) = match res {
+                    Ok(res) => res,
+                    Err(e) => {
+                        println!("{} failed: {}", arg, e);
+                        return ok(last);
+                    }
+                };
+                let mut out = format!("{}: {}", link, hash);
+                if let Some(last) = last {
+                    out = format!("{} ({})", out, distance(hash, last));
+                }
+                println!("{}", out);
 
-        Some(hash)
-    });
+                ok(Some(hash))
+            })
+        }).map(|_| ())
+    );
 }

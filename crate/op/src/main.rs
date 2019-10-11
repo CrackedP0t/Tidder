082b7dc8c4d16870c6e339a77b2370a80bf4ade3
Elaina Martineau <elainamartineau@gmail.com>
2019-10-10 22:21:41 -0600

    Add save to op

--- crate/op/src/main.rs
+++ crate/op/src/main.rs
@@ -2,18 +2,12 @@
 
 use clap::clap_app;
 use common::*;
-use failure::Error;
 use futures::prelude::*;
+use reqwest::{header::USER_AGENT, Client};
+use serde_json::Value;
+use serde::de::Deserialize;
 
-enum Op {
-    Post(String),
-    Hash(Vec<String>),
-}
-
-async fn post(id: String) -> Result<(), Error> {
-    use reqwest::{header::USER_AGENT, Client};
-    use serde_json::Value;
-
+async fn post(id: &str) -> Result<(), UserError> {
     let client = Client::new();
 
     let auth_resp = client
@@ -22,41 +16,96 @@ async fn post(id: String) -> Result<(), Error> {
             &SECRETS.reddit.client_id,
             Some(&SECRETS.reddit.client_secret),
         )
-        .query(&[
+        .form(&[
             ("grant_type", "password"),
             ("username", &SECRETS.reddit.username),
             ("password", &SECRETS.reddit.password),
         ])
         .send()
-        .await?
-        .error_for_status()?;
+        .await?;
 
+    let status = auth_resp.status();
     let json = auth_resp.json::<Value>().await?;
 
-    let access_token = json["access_token"]
-        .as_str()
-        .ok_or_else(|| format_err!("Access token not found"))?;
+    if status.is_success() {
+        let access_token = json["access_token"]
+            .as_str()
+            .ok_or_else(|| ue!("Access token not found"))?;
 
-    let link = format!("https://oauth.reddit.com/by_id/t3_{}", id);
+        let link = format!("https://oauth.reddit.com/by_id/t3_{}", id);
 
-    let resp = client
-        .get(&link)
-        .query(&[("raw_json", "1")])
-        .header(USER_AGENT, "Tidder 0.0.1")
-        .bearer_auth(access_token)
-        .send()
-        .await?
-        .error_for_status()?;
+        let resp = client
+            .get(&link)
+            .query(&[("raw_json", "1")])
+            .header(USER_AGENT, "Tidder 0.0.1")
+            .bearer_auth(access_token)
+            .send()
+            .await?
+            .error_for_status()?;
 
-    let post = &resp.json::<Value>().await?["data"]["children"][0]["data"];
+        let post = &resp.json::<Value>().await?["data"]["children"][0]["data"];
 
-    println!("{:#}", post);
+        println!("{:#}", post);
 
-    Ok(())
+        Ok(())
+    } else {
+        println!("{:#}", json);
+
+        Err(ue!(format!("Authentication HTTP error: {}", status)))
+    }
 }
 
-async fn hash(links: Vec<String>) -> Result<(), Error> {
-    futures::stream::iter(links.into_iter())
+async fn save(id: &str) -> Result<(), UserError> {
+    let client = Client::new();
+
+    let auth_resp = client
+        .post("https://www.reddit.com/api/v1/access_token")
+        .basic_auth(
+            &SECRETS.reddit.client_id,
+            Some(&SECRETS.reddit.client_secret),
+        )
+        .form(&[
+            ("grant_type", "password"),
+            ("username", &SECRETS.reddit.username),
+            ("password", &SECRETS.reddit.password),
+        ])
+        .send()
+        .await?;
+
+    let status = auth_resp.status();
+    let json = auth_resp.json::<Value>().await?;
+
+    if status.is_success() {
+        let access_token = json["access_token"]
+            .as_str()
+            .ok_or_else(|| ue!("Access token not found"))?;
+
+        let link = format!("https://oauth.reddit.com/by_id/t3_{}", id);
+
+        let resp = client
+            .get(&link)
+            .query(&[("raw_json", "1")])
+            .header(USER_AGENT, "Tidder 0.0.1")
+            .bearer_auth(access_token)
+            .send()
+            .await?
+            .error_for_status()?;
+
+        let post = Submission::deserialize(&resp.json::<Value>().await?["data"]["children"][0]["data"])?.finalize()?;
+
+        let (_hash, _hash_dest, image_id, _exists) = save_hash(&post.url, HashDest::Images).await?;
+
+        save_post(&post, Ok(image_id)).await?;
+        Ok(())
+    } else {
+        println!("{:#}", json);
+
+        Err(ue!(format!("Authentication HTTP error: {}", status)))
+    }
+}
+
+async fn hash(links: &[&str]) -> Result<(), UserError> {
+    futures::stream::iter(links.iter())
         .fold(None, async move |last, arg| -> Option<Hash> {
             let res = get_hash(&arg).await;
 
@@ -81,7 +130,9 @@ async fn hash(links: Vec<String>) -> Result<(), Error> {
     Ok(())
 }
 
-fn get_op() -> Result<Op, Error> {
+
+#[tokio::main]
+async fn main() -> Result<(), UserError> {
     let matches = clap_app!(op =>
         (@subcommand post =>
             (@arg ID: +required "Reddit's ID for the post")
@@ -89,33 +140,24 @@ fn get_op() -> Result<Op, Error> {
         (@subcommand hash =>
             (@arg LINKS: +required ... "The links you wish to hash")
         )
+        (@subcommand save =>
+            (@arg ID: +required ... "Reddit's ID for the post you wish to save")
+        )
     )
     .get_matches();
 
     let (op_name, op_matches) = matches.subcommand();
-    let op_matches = op_matches.ok_or_else(|| format_err!("No subcommand provided"))?;
+    let op_matches = op_matches.ok_or_else(|| ue!("No subcommand provided"))?;
 
-    let op = match op_name {
-        "post" => Op::Post(op_matches.value_of("ID").unwrap().to_string()),
-        "hash" => Op::Hash(
-            op_matches
+   match op_name {
+        "post" => post(op_matches.value_of("ID").unwrap()).await,
+        "hash" => hash(
+            &op_matches
                 .values_of("LINKS")
                 .unwrap()
-                .map(|l| l.to_owned())
-                .collect(),
-        ),
-        unknown => return Err(format_err!("Unknown subcommand '{}'", unknown)),
-    };
-
-    Ok(op)
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let op = get_op()?;
-
-    match op {
-        Op::Post(id) => post(id).await,
-        Op::Hash(links) => hash(links).await,
+                .collect::<Vec<_>>(),
+        ).await,
+        "save" => save(op_matches.value_of("ID").unwrap()).await,
+        unknown => Err(ue!(format!("Unknown subcommand '{}'", unknown))),
     }
 }

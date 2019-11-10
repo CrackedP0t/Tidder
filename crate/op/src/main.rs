6e1ec573e001318b85ffe4b45ef4844ecc585b53
Elaina Martineau <elainamartineau@gmail.com>
2019-11-10 02:29:03 -0700

    Print in op's hash

--- crate/op/src/main.rs
+++ crate/op/src/main.rs
@@ -108,6 +108,7 @@ async fn hash(links: &[&str]) -> Result<(), UserError> {
     futures::stream::iter(links.iter())
         .fold(None, move |last, arg| {
             async move {
+                println!("Starting to hash {}", arg);
                 let HashGotten { hash, end_link, .. } = match get_hash(&arg).await {
                     Ok(res) => res,
                     Err(e) => {

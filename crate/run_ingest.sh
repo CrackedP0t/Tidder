8be901fc0c49c1c1bf7af8f9d93c3c85f3bb35bd
Elaina Martineau <elainamartineau@gmail.com>
2019-11-10 01:44:24 -0700

    Pass arguments in run_ingest

--- crate/run_ingest.sh
+++ crate/run_ingest.sh
@@ -5,6 +5,6 @@ set -e
 ~/.cargo/bin/cargo build --bin ingest --release
 
 for URL in $(< ingest/todo.txt); do
-    target/release/ingest -D $URL
+    target/release/ingest $@ $URL
     tail -n +2 ingest/todo.txt | sponge ingest/todo.txt
 done

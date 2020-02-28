093e8ae341fc521da4a1c0cdb1f490ad3e09d911
Elaina Martineau <elainamartineau@gmail.com>
2020-02-27 18:11:51 -0700

    Vastly improve run_ingest

--- crate/run_ingest.sh
+++ crate/run_ingest.sh
@@ -7,10 +7,25 @@ for URL in $(< ~/tidder/crate/ingest/todo.txt); do
     cd ~/tidder/crate/
     ~/.cargo/bin/cargo build --bin ingest --release
     cd /mnt/permanent/archives
-    rm -f *
-    wget $URL
-    7z x *
-    RUST_LOG="info" ~/tidder/crate/target/release/ingest $@ !(*.*)
+
+    ARCHIVE="${URL##*/}"
+    UNPACK="${ARCHIVE%%.*}"
+    EXT="${UNPACK##*.}"
+
+    if [[ ! -e "$ARCHIVE" ]]; then
+        wget $URL
+    fi
+
+    if [[ ! -e "$UNPACK" ]]; then
+        if [[ "$EXT" == "zst" ]]; then
+            zstd -d "$ARCHIVE"
+        else
+            7z x "$ARCHIVE"
+        fi
+    fi
+
+    RUST_LOG="info" ~/tidder/crate/target/release/ingest $@ "$UNPACK"
     tail -n +2 ~/tidder/crate/ingest/todo.txt | sponge ~/tidder/crate/ingest/todo.txt
-    rm *
+
+    rm "$ARCHIVE" "$UNPACK"
 done

3d6adaf481e6e1faa6e8edbcb4be5ac48d74e3cb
Elaina Martineau <elainamartineau@gmail.com>
2020-07-06 00:42:26 -0600

    Simplify progress

--- crate/site/src/render.rs
+++ crate/site/src/render.rs
@@ -47,61 +47,6 @@ pub mod utils {
         arg.ok_or_else(|| Error::msg("Tester `null` was called on an undefined variable"))
             .map(Value::is_null)
     }
-
-    pub fn progress(_args: &HashMap<String, Value>) -> Result<Value> {
-        fn progress_res() -> Result<Value> {
-            const MONTHS: [&str; 12] = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December",
-            ];
-            static DATE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d\d\d\d)-(\d\d)").unwrap());
-
-            let file =
-                std::fs::File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/../ingest/todo.txt"))
-                    .map_err(Error::msg)?;
-            let mut buf_reader = std::io::BufReader::new(file);
-            let mut line = String::new();
-
-            buf_reader.read_line(&mut line).map_err(Error::msg)?;
-
-            let caps = DATE_RE
-                .captures(&line)
-                .ok_or_else(|| Error::msg("No date matched in line"))?;
-
-            Ok(format!(
-                "{} of {}",
-                MONTHS
-                    .get(
-                        caps.get(2)
-                            .ok_or_else(|| Error::msg("No month captured in line"))?
-                            .as_str()
-                            .parse::<usize>()
-                            .unwrap()
-                            - 1
-                    )
-                    .ok_or_else(|| Error::msg("Month out of range"))?,
-                caps.get(1)
-                    .ok_or_else(|| Error::msg("No year captured in line"))?
-                    .as_str(),
-            )
-            .into())
-        }
-
-        match progress_res() {
-            Err(_e) => Ok(Value::Null),
-            Ok(progress) => Ok(progress),
-        }
-    }
 }
 
 pub fn create_tera() -> Tera {
@@ -110,7 +55,6 @@ pub fn create_tera() -> Tera {
             t.register_filter("tern", utils::tern);
             t.register_filter("plural", utils::pluralize);
             t.register_tester("null", utils::null);
-            t.register_function("progress", utils::progress);
             t
         }
         Err(e) => {
